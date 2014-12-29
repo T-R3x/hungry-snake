38377c5cacd6bd6e0005b33ab17dca96121fcaa6
PhR3Ak <hofrichter.sascha@gmail.com>
2014-12-29 13:07:03 +0100

    initialize the grid system

--- js/Grid.js
+++ js/Grid.js
@@ -8,3 +8,21 @@ function Grid() {
     this.height = null;
     this.elements  = null;
 }
+
+/**
+ * Initialize the grid
+ */
+Grid.prototype.init = function (type, width, height) {
+    this.width = width;
+    this.height = height;
+
+    this.elements = [];
+
+    var x, y;
+    for (x = 0; x < width; x++) {
+        this.elements.push([]);
+        for (y = 0; y < height; y++) {
+            this.elements[x].push(type);
+        }
+    }
+};
