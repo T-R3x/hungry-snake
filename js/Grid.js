bbe1b1938eae6c11080c9d53c961926402eeac48
PhR3Ak <hofrichter.sascha@gmail.com>
2014-12-29 12:44:44 +0100

    Add JS-Classes of grid and snake

---
+++ js/Grid.js
@@ -0,0 +1,10 @@
+/**
+ * Grid constructor.
+ *
+ * @constructor
+ */
+function Grid() {
+    this.width  = null;
+    this.height = null;
+    this.elements  = null;
+}
---
+++ js/Snake.js
@@ -0,0 +1,10 @@
+/**
+ * Snake constructor.
+ *
+ * @constructor
+ */
+function Snake() {
+    this.direction = '';
+    this.head = null;
+    this.tailElements = [];
+}
